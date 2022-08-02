38fef38a182b731e5746ab39748bf15b85ea04cd
Ryane <gamuahryane@gmail.com>
2022-08-02 14:57:47 +0100

    New project to push to github

---
+++ Hello.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int arithmetic(int a,int b);
+cout<<"Enter the first number\n";
+cin>>a;
+cout<<"Enter the second number\n";
+cin>>b;
+j=a+b;
+return(j);
+int main()
+{
+	int num1,num2;
+	int answer(int num1,num2);
+	cout<<"the result gives %d\n",j;
+	return 0;
+}
